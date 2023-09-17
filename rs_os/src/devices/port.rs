1ccd20a6f4befa8e7e61ab7b8eea6af5b7c441f6
ajprabhu09 <ajinkyaprabhu97@gmail.com>
2023-09-17 01:37:49 -0400

    added port wrapper

---
+++ rs_os/src/devices/port.rs
@@ -0,0 +1,24 @@
+use crate::utils::asm::{self, iodelay};
+
+
+pub struct Port(u16);
+
+#[allow(dead_code)]
+impl Port {
+    pub fn send_byte(&self, val: u8) {
+        unsafe {
+            asm::outb(self.0, val);
+            iodelay();
+        }
+    }
+
+    pub fn read_byte(&self) -> u8 {
+        unsafe {
+            asm::inb(self.0)
+        }
+    }
+}
+
+
+
+
