1f1a5052d0ba95809836161207f894631bcfac27
ajprabhu09 <ajinkyaprabhu97@gmail.com>
2023-09-25 23:43:47 -0400

    added asm compilation

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,7 +1,29 @@
+use glob;
 fn main() {
     // This does not work yet!
-    cc::Build::new()
-        .flag("--target=x86_64-unknown-none")
-        .file("cc/test.c")
-        .compile("foo");
+    println!("cargo:rerun-if-changed=cc/src/*.c");
+    println!("cargo:rerun-if-changed=cc/inc/*.h");
+    println!("cargo:rerun-if-changed=cc/asm/*.s");
+    let include_path = "cc/inc/";
+    let mut build = cc::Build::new();
+
+    for entry in glob::glob("cc/src/*.c").expect("invalid path cc/src/ ") {
+        if let Ok(path) = entry {
+            build.flag("--target=x86_64-unknown-none")
+            .file(path.display().to_string())
+            .include(include_path);
+        }
+    }
+    for entry in glob::glob("cc/asm/*.s").expect("invalid path cc/asm/ ") {
+        if let Ok(path) = entry {
+            build
+            .flag("--target=x86_64-unknown-none")
+            .file(path.display().to_string())
+            .include(include_path);
+        }
+    }
+    build.compile("ajinuxcc");
+
+
+
 }
--- kernel/src/cc/mod.rs
+++ kernel/src/cc/mod.rs
@@ -1,3 +1,4 @@
 extern "C" {
     pub fn test_func() -> i32;
+    pub fn func() -> i32;
 }
