229bc329d45c4b24169fda5e059566a0472438e1
ajprabhu09 <ajinkyaprabhu97@gmail.com>
2023-12-06 18:17:26 -0500

    setup user tests build

---
+++ user_tests/build.rs
@@ -0,0 +1,25 @@
+use std::process::Command;
+
+fn main() {
+    // This does not work yet!
+    println!("cargo:rerun-if-changed=../user/tests/progs/*.c");
+    // println!("cargo:rerun-if-changed=cc/inc/*.h");
+    // println!("cargo:rerun-if-changed=cc/asm/*.s");
+    // let x: i32 = "1asd".parse().unwrap();
+    // panic!("hehehe");
+
+
+    let x = Command::new("/usr/bin/make")
+        .arg("-f")
+        .arg("../user/tests/Makefile")
+        .arg("clean")
+        .output()
+        .expect("failed to build user tests");
+
+    let x = Command::new("make")
+        .arg("-f")
+        .arg("../user/tests/Makefile")
+        .output()
+        .expect("failed to build user tests");
+    // build.compile("ajinuxcc");
+}
--- user_tests/src/lib.rs
+++ user_tests/src/lib.rs
@@ -1,14 +1,12 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![feature(allocator_api)]
+#![feature(abi_x86_interrupt)]
+#![feature(const_mut_refs)]
+#![allow(clippy::empty_loop)]
+#![allow(clippy::needless_return)]
+#![feature(let_chains)]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
